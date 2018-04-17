860e4f22f4f33c8da559a08c430e8f72991e1472
waichungwong <johnwongwwc@gmail.com>
2018-04-17 22:48:58 +0100

    removed unused modules

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { Provider } from "react-redux";
 
-import { initialize } from "./controllers";
-import store from "./store";
 import App from "./app";
 
-initialize();
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+render(<App />, document.getElementById("root"));
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import connection from "./reducer.connection";
-
-export default combineReducers({
-  connection
-});
